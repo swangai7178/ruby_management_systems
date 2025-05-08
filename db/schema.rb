8fdfc75102f415f6884b9046213234db2e03c828
swangai7178 <swangai7178@gmail.com>
2025-05-08 18:07:50 +0300

    versioning fix for smartply for both android and ios

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_06_055047) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_08_145544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "sku"
+    t.decimal "price"
+    t.text "description"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
